013c08ad8f13a1bb38f2081768b3ba573088a778
Markus Budeus <ge25joh@mytum.de>
2023-11-14 12:50:56 +0100

    WIP on Company Export

--- src/main/java/de/tum/med/aiim/markusbudeus/fhirexporter/neo4j/Neo4jOrganizationExporter.java
+++ src/main/java/de/tum/med/aiim/markusbudeus/fhirexporter/neo4j/Neo4jOrganizationExporter.java
@@ -3,12 +3,14 @@ package de.tum.med.aiim.markusbudeus.fhirexporter.neo4j;
 import de.tum.med.aiim.markusbudeus.fhirexporter.resource.CodeableConcept;
 import de.tum.med.aiim.markusbudeus.fhirexporter.resource.Coding;
 import de.tum.med.aiim.markusbudeus.fhirexporter.resource.Identifier;
+import de.tum.med.aiim.markusbudeus.fhirexporter.resource.organization.Address;
 import de.tum.med.aiim.markusbudeus.fhirexporter.resource.organization.Organization;
 import de.tum.med.aiim.markusbudeus.fhirexporter.resource.substance.Substance;
 import org.neo4j.driver.Query;
 import org.neo4j.driver.Record;
 import org.neo4j.driver.Result;
 import org.neo4j.driver.Session;
+import org.neo4j.driver.types.MapAccessorWithDefaultValue;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -27,11 +29,65 @@ public class Neo4jOrganizationExporter {
 	 * Reads all manufacturers from the database and returns them as a stream of {@link Organization Organizations}.
 	 */
 	public Stream<Organization> loadOrganizations() {
+
+		Result result = session.run(new Query(
+				"MATCH (s:" + COMPANY_LABEL + ") " +
+						"OPTIONAL MATCH (s)-[:" + COMPANY_HAS_ADDRESS_LABEL + "]-(a:" + ADDRESS_LABEL + ") " +
+						"RETURN s.mmiId, s.name, s.shortName, collect({" +
+						"street:a.street," +
+						"streetNumber:a.streetNumber," +
+						"postalCode:a.postalCode," +
+						"city:a.city," +
+						"country:a.country," +
+						"countryCode:a.countryCode" +
+						"}) as addresses"
+		));
 		return null;
 	}
 
 	private static Organization toOrganization(Record record) {
+		Neo4jExportOrganisation exportOrganisation = new Neo4jExportOrganisation(record);
+
+		Organization organization = new Organization();
+		organization.active = true;
+		Address address = new Address();
+		address.setUse(Address.Use.WORK);
+
+		// TODO
 		return null;
+	}
+
+	private static class Neo4jExportOrganisation {
+		final long mmiId;
+		final String name;
+		final String shortName;
+		final List<Neo4jExportAddress> addresses;
+
+		Neo4jExportOrganisation(MapAccessorWithDefaultValue value) {
+			mmiId = value.get("mmiId").asLong();
+			name = value.get("name", (String) null);
+			shortName = value.get("shortName", (String) null);
+			addresses = value.get("addresses").asList(Neo4jExportAddress::new);
+		}
+	}
+
+	private static class Neo4jExportAddress {
+		final String street;
+		final String streetNumber;
+		final String postalCode;
+		final String city;
+		final String country;
+		final String countryCode;
+
+		public Neo4jExportAddress(MapAccessorWithDefaultValue value) {
+			street = value.get("street", (String) null);
+			streetNumber = value.get("streetNumber", (String) null);
+			postalCode = value.get("postalCode", (String) null);
+			city = value.get("city", (String) null);
+			country = value.get("country", (String) null);
+			countryCode = value.get("countryCode", (String) null);
+		}
+
 	}
 
 }
