4ec36cecda9df6621f1c2c02d7a2d6a630118ba3
Markus Budeus <markus.budeus@tum.de>
2024-05-10 15:37:21 +0200

    Reintroduced the SubSortingTree and added tests

--- medgraph/build.gradle.kts
+++ medgraph/build.gradle.kts
@@ -10,6 +10,8 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jetbrains:annotations:24.0.0")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
