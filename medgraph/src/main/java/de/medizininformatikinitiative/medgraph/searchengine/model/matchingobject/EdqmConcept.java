e211d02a4d30f9da77bf742b7add77b78f8911a0
Markus Budeus <markus.budeus@tum.de>
2024-05-29 15:08:06 +0200

    Started introducing EDQM Dose Forms to the Search Algorithm

---
+++ medgraph/src/main/java/de/medizininformatikinitiative/medgraph/searchengine/model/matchingobject/EdqmConcept.java
@@ -0,0 +1,72 @@
+package de.medizininformatikinitiative.medgraph.searchengine.model.matchingobject;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+/**
+ * Represents a concept from the EDQM Standard terms.
+ *
+ * @author Markus Budeus
+ */
+public class EdqmConcept implements Identifiable {
+
+	/**
+	 * The code (including a class-specific prefix, i.e. PDF-00000001) of this concept.
+	 */
+	@NotNull
+	private final String code;
+
+	/**
+	 * The official (english) name of this concept.
+	 */
+	@NotNull
+	private final String name;
+
+	/**
+	 * The type of this Standard Terms concept. (E.g. Pharmaceutical dose dorm, Release characteristic)
+	 */
+	@NotNull
+	private final String conceptType;
+
+	public EdqmConcept(@NotNull String code, @NotNull String name, @NotNull String conceptType) {
+		this.code = code;
+		this.name = name;
+		this.conceptType = conceptType;
+	}
+
+	@NotNull
+	public String getCode() {
+		return code;
+	}
+
+	@Override
+	@NotNull
+	public String getName() {
+		return name;
+	}
+
+	@NotNull
+	public String getConceptType() {
+		return conceptType;
+	}
+
+	@Override
+	public String toString() {
+		return name;
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (this == object) return true;
+		if (object == null || getClass() != object.getClass()) return false;
+		EdqmConcept that = (EdqmConcept) object;
+		return Objects.equals(code, that.code) && Objects.equals(name,
+				that.name) && Objects.equals(conceptType, that.conceptType);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(code);
+	}
+}
