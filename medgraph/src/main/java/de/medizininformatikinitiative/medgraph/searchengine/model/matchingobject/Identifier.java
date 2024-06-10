e017ccf5a5888b5d714a122094c19fd37f733d56
Markus Budeus <markus.budeus@tum.de>
2024-06-10 10:44:33 +0200

    Fixed DoseDetector failing tests

---
+++ medgraph/src/main/java/de/medizininformatikinitiative/medgraph/searchengine/model/matchingobject/Identifier.java
@@ -0,0 +1,10 @@
+package de.medizininformatikinitiative.medgraph.searchengine.model.matchingobject;
+
+/**
+ * @author Markus Budeus
+ */
+public interface Identifier<T> {
+
+	T getIdentifier();
+
+}
