aabde55aa6339ca84b534f6bd679f7cbc1fdb6b7
Markus Budeus <ge25joh@mytum.de>
2024-06-13 11:40:35 +0200

    Added an option to the UI which allows you to track how the parsed query came to be

--- composeApp/src/commonMain/kotlin/de/medizininformatikinitiative/medgraph/ui/searchengine/pipeline/PipelineInfoDialog.kt
+++ composeApp/src/commonMain/kotlin/de/medizininformatikinitiative/medgraph/ui/searchengine/pipeline/PipelineInfoDialog.kt
@@ -2,6 +2,7 @@ package de.medizininformatikinitiative.medgraph.ui.searchengine.pipeline
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -12,17 +13,42 @@ import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.NonRestartableComposable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
+import de.medizininformatikinitiative.medgraph.searchengine.model.identifiable.Identifiable
+import de.medizininformatikinitiative.medgraph.searchengine.model.identifier.Identifier
 import de.medizininformatikinitiative.medgraph.searchengine.model.matchingobject.MatchingObject
+import de.medizininformatikinitiative.medgraph.searchengine.provider.MappedIdentifier
 import de.medizininformatikinitiative.medgraph.ui.resources.StringRes
+import de.medizininformatikinitiative.medgraph.ui.searchengine.pipeline.identifier.IdentifierUI
 import de.medizininformatikinitiative.medgraph.ui.searchengine.pipeline.matchingobject.MatchingObjectPipelineDisplay
 
 @Composable
+@NonRestartableComposable
+fun PipelineInfoDialog(identifier: MappedIdentifier<*>, onDismissRequest: () -> Unit) {
+    PipelineInfoDialog(identifier.identifier, onDismissRequest, identifier.target)
+}
+
+@Composable
+fun PipelineInfoDialog(identifier: Identifier<*>, onDismissRequest: () -> Unit, identifierTarget: Identifiable? = null) {
+    PipelineInfoDialog(onDismissRequest) {
+        IdentifierUI(identifier, modifier = Modifier.fillMaxWidth(), identifierTarget)
+    }
+}
+
+@Composable
 fun PipelineInfoDialog(target: MatchingObject<*>, onDismissRequest: () -> Unit) {
+    PipelineInfoDialog(onDismissRequest) {
+        MatchingObjectPipelineDisplay(target, modifier = Modifier.fillMaxWidth())
+    }
+}
+
+@Composable
+fun PipelineInfoDialog(onDismissRequest: () -> Unit, content: @Composable ColumnScope.() -> Unit) {
     Dialog(onDismissRequest, DialogProperties(
         dismissOnBackPress = true,
         dismissOnClickOutside = true,
@@ -39,7 +65,7 @@ fun PipelineInfoDialog(target: MatchingObject<*>, onDismissRequest: () -> Unit) 
             Text(StringRes.pipeline_info_subtitle, style = MaterialTheme.typography.subtitle1)
             Divider(modifier = Modifier.fillMaxWidth(), thickness = 2.dp)
             Spacer(modifier = Modifier.height(4.dp))
-            MatchingObjectPipelineDisplay(target, modifier = Modifier.fillMaxWidth())
+            content()
         }
 
     }
