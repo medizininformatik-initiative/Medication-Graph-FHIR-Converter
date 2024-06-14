ac53d3164b05857362d8c3d415326fab5542e688
Markus Budeus <markus.budeus@tum.de>
2024-06-14 11:57:01 +0200

    Increased the size of menu button texts

--- composeApp/src/desktopMain/kotlin/de/medizininformatikinitiative/medgraph/ui/MainMenu.desktop.kt
+++ composeApp/src/desktopMain/kotlin/de/medizininformatikinitiative/medgraph/ui/MainMenu.desktop.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
 import cafe.adriel.voyager.navigator.Navigator
 import de.medizininformatikinitiative.medgraph.ui.desktop.fhirexporter.FhirExporterScreen
 import de.medizininformatikinitiative.medgraph.ui.desktop.graphdbpopulator.GraphDbPopulatorScreen
@@ -13,18 +14,19 @@ import de.medizininformatikinitiative.medgraph.ui.theme.templates.Button
 @Composable
 actual fun ColumnScope.MainMenuExtensions(
     navigator: Navigator,
-    navigationButtonDesign: Modifier
+    navigationButtonDesign: Modifier,
+    buttonTextStyle: TextStyle
 ) {
     Button(
         { navigator.push(GraphDbPopulatorScreen()) },
         modifier = navigationButtonDesign
     ) {
-        Text(StringRes.main_menu_populate_database)
+        Text(StringRes.main_menu_populate_database, style = buttonTextStyle)
     }
     Button(
         { navigator.push(FhirExporterScreen()) },
         modifier = navigationButtonDesign
     ) {
-        Text(StringRes.main_menu_fhir_exporter)
+        Text(StringRes.main_menu_fhir_exporter, style = buttonTextStyle)
     }
 }
