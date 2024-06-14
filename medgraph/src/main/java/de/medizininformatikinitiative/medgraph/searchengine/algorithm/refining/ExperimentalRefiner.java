5d0f85f1ab53ad031996e6e780a21df6508c41f8
Markus Budeus <markus.budeus@tum.de>
2024-06-14 16:04:48 +0200

    Caused the ExperimentalRefiner to unsafely force a sorting based on the initial match's score to prioritize best name matches

--- medgraph/src/main/java/de/medizininformatikinitiative/medgraph/searchengine/algorithm/refining/ExperimentalRefiner.java
+++ medgraph/src/main/java/de/medizininformatikinitiative/medgraph/searchengine/algorithm/refining/ExperimentalRefiner.java
@@ -2,10 +2,11 @@ package de.medizininformatikinitiative.medgraph.searchengine.algorithm.refining;
 
 import de.medizininformatikinitiative.medgraph.searchengine.algorithm.querymanagement.RefinedQuery;
 import de.medizininformatikinitiative.medgraph.searchengine.db.Database;
+import de.medizininformatikinitiative.medgraph.searchengine.matcher.model.ScoreBasedMatch;
 import de.medizininformatikinitiative.medgraph.searchengine.model.SearchQuery;
 import de.medizininformatikinitiative.medgraph.searchengine.model.identifiable.Product;
 import de.medizininformatikinitiative.medgraph.searchengine.model.identifiable.Substance;
-import de.medizininformatikinitiative.medgraph.searchengine.model.matchingobject.MatchingObject;
+import de.medizininformatikinitiative.medgraph.searchengine.model.matchingobject.*;
 import de.medizininformatikinitiative.medgraph.searchengine.pipeline.OngoingRefinement;
 import de.medizininformatikinitiative.medgraph.searchengine.pipeline.judge.ProductOnlyFilter;
 import de.medizininformatikinitiative.medgraph.searchengine.pipeline.judge.dosage.DosageAndAmountInfoMatchJudge;
@@ -14,14 +15,14 @@ import de.medizininformatikinitiative.medgraph.searchengine.pipeline.judge.dosef
 import de.medizininformatikinitiative.medgraph.searchengine.pipeline.judge.doseform.PharmaceuticalDoseFormJudge;
 import de.medizininformatikinitiative.medgraph.searchengine.pipeline.transformer.ProductDetailsResolver;
 import de.medizininformatikinitiative.medgraph.searchengine.pipeline.transformer.SubstanceToProductResolver;
+import de.medizininformatikinitiative.medgraph.searchengine.pipeline.tree.ScoreSortDirective;
 import de.medizininformatikinitiative.medgraph.searchengine.pipeline.tree.SubSortingTree;
 import org.neo4j.driver.Session;
 
 import java.util.List;
 
 /**
- * The (as of 2024-05-14) one and only {@link MatchRefiner}-implementation and the one I am
- * experimenting with.
+ * The (as of 2024-05-14) one and only {@link MatchRefiner}-implementation and the one I am experimenting with.
  *
  * @author Markus Budeus
  */
@@ -55,7 +56,7 @@ public class ExperimentalRefiner implements MatchRefiner {
 	 */
 	@Override
 	public SubSortingTree<MatchingObject<?>> refineMatches(List<? extends MatchingObject<?>> initialMatches,
-	                                                    RefinedQuery query) {
+	                                                       RefinedQuery query) {
 		SearchQuery searchQuery = query.toSearchQuery();
 
 		// Use the substances from the search query to resolve products
@@ -65,6 +66,8 @@ public class ExperimentalRefiner implements MatchRefiner {
 
 		// Merge the products resolved from substances with the products from the initial search
 		SubSortingTree<MatchingObject<?>> productMatches = new SubSortingTree<>(initialMatches);
+		forceSortByMatchScore(productMatches);
+
 		SubSortingTree<MatchingObject<?>> substanceMatches = substanceBasedSearch.getCurrentMatchesTree();
 		SubSortingTree<MatchingObject<?>> combinedMatches = SubSortingTree.merge(productMatches, substanceMatches);
 		combinedMatches.clearDuplicates();
@@ -87,6 +90,41 @@ public class ExperimentalRefiner implements MatchRefiner {
 		matching.applyScoreJudge(dosagesInProductNameJudge, false);
 
 		return matching.getCurrentMatchesTree();
+	}
+
+	// TODO Below is unsafe and needs a redesign. This whole mechanism needs one. Remove the initialmatchfinder, it makes
+	//  no sense to separate that.
+
+	/**
+	 * <b>This only works if all elements in the given tree originate from a score-based matcher!</b>
+	 * Sorts all elements in the tree based on the score assigned by the score based matcher.
+	 */
+	private	void forceSortByMatchScore(SubSortingTree<MatchingObject<?>> tree){
+		tree.applySortingStep(new ScoreSortDirective<>("Product Edit Distance",
+				this::forceAcquireScore,null));
+	}
+
+	private double forceAcquireScore(MatchingObject<?> matchingObject) {
+		if (matchingObject instanceof OriginalMatch<?> m) {
+			return forceAcquireScore(m);
+		} else if (matchingObject instanceof Merge<?> m) {
+			double maxScore = 0;
+			for (MatchingObject<?> source: m.getSourceObjects()) {
+				double score = forceAcquireScore(source);
+				if (score > maxScore) {
+					maxScore = score;
+				}
+			}
+			return maxScore;
+		} else if (matchingObject instanceof TransformedObject<?,?> t){
+			return forceAcquireScore(t.getSourceObject());
+		}
+		return 0;
+	}
+
+	private double forceAcquireScore(OriginalMatch<?> originalMatch) {
+		MatchOrigin<? extends ScoreBasedMatch<?, ?>> origin = (MatchOrigin<? extends ScoreBasedMatch<?,?>>) originalMatch.getOrigin();
+		return origin.getMatch().getScore();
 	}
 
 }
