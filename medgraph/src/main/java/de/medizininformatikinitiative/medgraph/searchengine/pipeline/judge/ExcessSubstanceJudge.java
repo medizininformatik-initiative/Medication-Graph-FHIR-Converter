1c7f6104024066387afedf31918fcfd2a9ee7ed6
Markus Budeus <markus.budeus@tum.de>
2024-11-28 14:07:03 +0100

    Modified the ExcessSubstanceJudge to assign no penalty if substances are not part of the search

--- medgraph/src/main/java/de/medizininformatikinitiative/medgraph/searchengine/pipeline/judge/ExcessSubstanceJudge.java
+++ medgraph/src/main/java/de/medizininformatikinitiative/medgraph/searchengine/pipeline/judge/ExcessSubstanceJudge.java
@@ -2,7 +2,9 @@ package de.medizininformatikinitiative.medgraph.searchengine.pipeline.judge;
 
 import de.medizininformatikinitiative.medgraph.searchengine.model.SearchQuery;
 import de.medizininformatikinitiative.medgraph.searchengine.model.identifiable.DetailedProduct;
+import de.medizininformatikinitiative.medgraph.searchengine.model.identifiable.Substance;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -11,18 +13,31 @@ import java.util.Set;
  * This judge assigns a negative score based on how many distinct active substances a product contains which are not
  * part of the search query. The idea is that if a user searches for a set of active ingredients, drugs which contain
  * exactly these ingredients are preferred over those that contain more ingredients.
+ * <p>
+ * If no substances are part of the search query, this judge always assigns a score of 0.
  *
  * @author Markus Budeus
  */
 public class ExcessSubstanceJudge extends ScoreJudge<DetailedProduct> {
 
 	@Override
+	protected List<Double> batchJudgeInternal(List<? extends DetailedProduct> matchables, SearchQuery query) {
+		// Abort quickly if no substances are being searched
+		if (query.getSubstances().isEmpty()) {
+			return Collections.nCopies(matchables.size(), 0.0);
+		}
+		return super.batchJudgeInternal(matchables, query);
+	}
+
+	@Override
 	protected double judgeInternal(DetailedProduct matchable, SearchQuery query) {
+		List<Substance> querySubstances = query.getSubstances();
+		if (querySubstances.isEmpty()) return 0.0;
 		Set<String> substanceNames = new HashSet<>();
 		matchable.getDrugs().forEach(d ->
 				d.activeIngredients().forEach(ingredient -> substanceNames.add(ingredient.getSubstanceName()))
 		);
-		query.getSubstances().forEach(s -> substanceNames.remove(s.getName()));
+		querySubstances.forEach(s -> substanceNames.remove(s.getName()));
 		return -substanceNames.size();
 	}
 
