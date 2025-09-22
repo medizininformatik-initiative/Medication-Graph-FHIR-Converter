7225ab25f04d2b6eef115bd63b8fdba4071778a7
Markus Budeus <markus.budeus@tum.de>
2025-09-22 17:00:56 +0200

    Migrated Organization implementation to HAPI

---
+++ medgraph/src/main/java/de/medizininformatikinitiative/medgraph/fhirexporter/neo4j/IdProvider.java
@@ -0,0 +1,31 @@
+package de.medizininformatikinitiative.medgraph.fhirexporter.neo4j;
+
+/**
+ * Central provider for FHIR medication resource IDs.
+ *
+ * @author Markus Budeus
+ */
+public class IdProvider {
+
+	private static final String PREFIX = "MII-DIZ-Medication-Conv-MMI-";
+	private static final String PRODUCT_PREFIX = PREFIX + "P-";
+	private static final String SUBSTANCE_PREFIX = PREFIX + "S-";
+	private static final String ORGANIZATION_PREFIX = PREFIX + "O-";
+
+	public static String combinedMedicalProductSubproductIdentifier(long parentMmiId, int childNo) {
+		return fromProductMmiId(parentMmiId) + "-" + childNo;
+	}
+
+	public static String fromOrganizationMmiId(long mmiId) {
+		return ORGANIZATION_PREFIX + mmiId;
+	}
+
+	public static String fromSubstanceMmiId(long mmiId) {
+		return SUBSTANCE_PREFIX + mmiId;
+	}
+
+	public static String fromProductMmiId(long productMmiId) {
+		return PRODUCT_PREFIX + productMmiId;
+	}
+
+}
